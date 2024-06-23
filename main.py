5fe1613c9f980cc3a09cc4578a63b44977d68171
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-06-23 18:34:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
-    os.chdir(r"D:\03) Project\data analysis project")
     df = pd.read_csv("Superstore.csv", encoding = "ISO-8859-1")
 
 col1, col2 = st.columns((2))
